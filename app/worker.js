20f2cf388465ad1273ec41ca4400a4634126f5ec
Jonathan Arbely <dev@jonathanarbely.de>
2021-02-02 23:03:44 +0100

    Cookieconsent deactivated and various js improvements

--- app/worker.js
+++ app/worker.js
@@ -1,7 +1,7 @@
 /* jslint node: true */
 "use strict";
 console.log("WORKER: executing.");
-var version = "v26::",
+var version = "v27::",
     offlineFundamentals = ['',
                             'assets/stylesheets/artofmagellan.css',
                             'assets/stylesheets/pagespecific/index.css',
